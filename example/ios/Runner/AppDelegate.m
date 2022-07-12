1d8f29fa42834817838483773f5d4dedb5a03c17
palash <palash.pandya592@gmail.com>
2022-07-12 17:29:18 +0530

    - created a test method channel for iOS. I tried to add an existing iOS module to the plugin.

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -3,10 +3,30 @@
 
 @implementation AppDelegate
 
-- (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+
+  FlutterMethodChannel* starPrinterChannel = [FlutterMethodChannel
+                                          methodChannelWithName:@"starPrinter"
+                                          binaryMessenger:controller.binaryMessenger];
+
+  [starPrinterChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+
+      if ([call.method  isEqual: @"searchPrinter"]) {
+          result(@("searchPrinter"));
+      }else if([call.method  isEqual: @"getPrinter"]){
+          result(@("getPrinter"));
+      }else if([call.method  isEqual: @"createReceipt"]){
+          result(@("createReceipt"));
+      }else if([call.method  isEqual: @"changeStyle"]){
+          result(@("changeStyle"));
+      }else if([call.method  isEqual: @"printReceipt"]){
+          result(@("printReceipt"));
+      }
+
+  }];
+
   [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
